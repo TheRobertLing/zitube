fd4cbcd67c2f025afc099f1f04c4922385c11407
Leo Tong <leo.tong@student.unsw.edu.au>
2025-03-31 19:52:48 +1100

    created navbar

--- src/components/navbar/NavBarComponent.vue
+++ src/components/navbar/NavBarComponent.vue
@@ -3,20 +3,41 @@ import Toolbar from '@/volt/Toolbar.vue'
 import Button from '@/volt/Button.vue'
 import SecondaryButton from '@/volt/SecondaryButton.vue'
 import InputText from '@/volt/InputText.vue'
-import { ref } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 
 const userSearch = ref<string>('')
+const showMobileSearch = ref<boolean>(false)
+
+const openMobileSearch = (): void => {
+  showMobileSearch.value = true
+}
+const closeMobileSearch = (): void => {
+  showMobileSearch.value = false
+}
+
+const handleResize = (): void => {
+  if (window.innerWidth >= 640) {
+    showMobileSearch.value = false
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize)
+})
 </script>
 
 <template>
-  <Toolbar>
+  <Toolbar v-show="!showMobileSearch" pt:center="grow justify-center" >
     <template #start>
-      <Button label="ZiTube" variant="text" pt:root="text-xl"></Button>
+      <Button label="ZiTube" variant="text"></Button>
     </template>
 
     <template #center>
-      <!-- Wide screen -->
-      <div class="relative hidden sm:block">
+      <div class="relative hidden sm:block max-w-4xl grow">
         <i
           class="pi pi-search absolute top-1/2 -mt-2 text-surface-400 leading-none start-3 z-1"
         ></i>
@@ -25,20 +46,46 @@ const userSearch = ref<string>('')
           v-model="userSearch"
           variant="filled"
           placeholder="Search"
-          pt:root="ps-10 "
+          pt:root="ps-10 w-full"
         />
-      </div>
-
-      <!-- Mobile Screen -->
-      <div>
-
       </div>
     </template>
 
     <template #end>
-      <SecondaryButton icon="pi pi-search" variant="text" rounded pt:root="sm:hidden" />
+      <SecondaryButton
+        icon="pi pi-search"
+        variant="text"
+        rounded
+        pt:root="sm:hidden"
+        @click="openMobileSearch"
+      />
       <SecondaryButton label="Log In" variant="text" />
       <SecondaryButton label="Sign Up" variant="text" />
+    </template>
+  </Toolbar>
+  <Toolbar v-show="showMobileSearch" pt:center="grow justify-center">
+    <template #start>
+      <SecondaryButton
+        icon="pi pi-arrow-left"
+        variant="text"
+        rounded
+        @click="closeMobileSearch"
+      />
+    </template>
+
+    <template #center>
+      <div class="relative max-w-4xl grow">
+        <i
+          class="pi pi-search absolute top-1/2 -mt-2 text-surface-400 leading-none start-3 z-1"
+        ></i>
+        <InputText
+          type="text"
+          v-model="userSearch"
+          variant="filled"
+          placeholder="Search"
+          pt:root="ps-10 w-full"
+        />
+      </div>
     </template>
   </Toolbar>
 </template>
