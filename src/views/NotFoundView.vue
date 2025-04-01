138873cfdf7bd42692d1ffe4fbe454e7a78855c9
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-01 23:45:19 +1100

    added page not found route

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
-
+import { Button, Card } from 'primevue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div>
-
+  <div class="w-full min-h-[calc(100vh-68px)] flex justify-center items-center">
+    <Card style="width: 25rem; overflow: hidden" pt:title="text-center">
+      <template #header>
+        <img alt="Page Not Found" src="@/assets/PageNotFound.gif" class="w-full" />
+      </template>
+      <template #title>Page Not Found!</template>
+      <template #content>
+        <p class="m-0 text-center">
+          Oops, it seems like this page is missing. This is most likely due to the URL being
+          incorrect or if the page is still undergoing maintenance. Apologies for any inconvenience
+          caused.
+        </p>
+      </template>
+      <template #footer>
+        <div class="flex gap-4 mt-1">
+          <RouterLink to="/" class="w-full">
+            <Button label="Return to Feed" severity="primary" outlined class="w-full"> </Button>
+          </RouterLink>
+        </div>
+      </template>
+    </Card>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
