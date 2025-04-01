dc2e9b1ba23c5e4e7583424da88e8c1d052a002e
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-01 21:33:55 +1100

    defined the main feed page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import CategorySliderComponent from '@/components/home/CategorySliderComponent.vue'
+import VideoFeedComponent from '@/components/home/VideoFeedComponent.vue'
+
+const selectedCategory = ref<string>('All')
+
+const handleChipSelect = (chip: string): void => {
+  selectedCategory.value = chip
+}
 
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <CategorySliderComponent :category="selectedCategory" @chipSelect="handleChipSelect"></CategorySliderComponent>
+  <VideoFeedComponent :category="selectedCategory"></VideoFeedComponent>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
