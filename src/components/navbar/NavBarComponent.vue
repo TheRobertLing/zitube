2ca65773a3e0a686a06443591d54a8894e1d4740
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-01 23:45:06 +1100

    fixed searchbar no blurring when entered

--- src/components/navbar/NavBarComponent.vue
+++ src/components/navbar/NavBarComponent.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { Toolbar, Button, IconField, InputIcon, InputText } from 'primevue'
-import { onBeforeUnmount, onMounted, ref } from 'vue'
+import { onBeforeUnmount, onMounted, ref, useTemplateRef } from 'vue'
+import { RouterLink, useRouter } from 'vue-router'
 
+const focusDiv = useTemplateRef('focusHere')
 const userSearch = ref<string>('')
 const showMobileSearch = ref<boolean>(false)
+const router = useRouter()
 
 const openMobileSearch = (): void => {
   showMobileSearch.value = true
@@ -18,6 +21,21 @@ const handleResize = (): void => {
   }
 }
 
+const handleSearch = (e: KeyboardEvent): void => {
+  if (e.key === 'Enter') {
+    router.push({ name: 'search', query: { search: userSearch.value } })
+    console.log(e)
+    if (focusDiv.value) {
+      console.log('This worked')
+      focusDiv.value.focus()
+    }
+  }
+}
+
+const handleClick = (): void => {
+  router.push({ name: 'search', query: { search: userSearch.value } })
+}
+
 onMounted(() => {
   window.addEventListener('resize', handleResize)
 })
@@ -28,15 +46,18 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
+  <!-- Hidden div to fix focus issue  -->
+  <div ref="focusHere" tabindex="0" class="absolute w-0 h-0 opacity-0 overflow-hidden"></div>
+
   <Toolbar v-show="!showMobileSearch" pt:center="grow justify-center" pt:root="sticky top-0 z-10">
     <template #start>
-      <Button label="ZiTube" variant="text"></Button>
+      <RouterLink to="/" class="w-full"><Button label="ZiTube" variant="text"></Button></RouterLink>
     </template>
 
     <template #center>
       <IconField pt:root="hidden md:block max-w-2xl grow">
-        <InputIcon class="pi pi-search" />
-        <InputText v-model="userSearch" placeholder="Search" variant="filled" fluid />
+        <InputIcon class="pi pi-search hover:cursor-pointer" :onclick="handleClick" />
+        <InputText v-model="userSearch" placeholder="Search" fluid :onKeydown="handleSearch" />
       </IconField>
     </template>
 
@@ -63,8 +84,8 @@ onBeforeUnmount(() => {
 
     <template #center>
       <IconField pt:root="grow">
-        <InputIcon class="pi pi-search" />
-        <InputText v-model="userSearch" placeholder="Search" variant="filled" fluid />
+        <InputIcon class="pi pi-search hover:cursor-pointer" :onclick="handleClick" />
+        <InputText v-model="userSearch" placeholder="Search" fluid :onKeydown="handleSearch" />
       </IconField>
     </template>
   </Toolbar>
