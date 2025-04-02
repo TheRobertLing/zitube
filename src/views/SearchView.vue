e3e71c892a160c0f1d64e3336df625c1f673c311
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-03 01:26:34 +1100

    started drafting

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
-
+import SearchFiltersComponent from '@/components/search/SearchFiltersComponent.vue'
+import SearchResultsFeedComponent from '@/components/search/SearchResultsFeedComponent.vue'
 </script>
 
 <template>
   <div>
+    <!-- Filters -->
+    <SearchFiltersComponent />
 
+    <!-- Vertical Feed -->
+    <SearchResultsFeedComponent />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
