622fd2d5d14e642a5cbd82b9c12c3938ee630f5a
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-03 18:39:45 +1100

    implemented search results feed

--- src/components/search/SearchResultsFeedComponent.vue
+++ src/components/search/SearchResultsFeedComponent.vue
@@ -1,17 +1,61 @@
 <script setup lang="ts">
-import WideVideoCardComponent from './WideVideoCardComponent.vue';
+import type { VideoCardInterface } from '@/types/VideoCardInterface'
+import WideVideoCardComponent from './WideVideoCardComponent.vue'
+import { useRouter } from 'vue-router';
 
+/**
+ * =============================================================================
+ *
+ * PROPS AND EMITS
+ *
+ * =============================================================================
+ */
 
+defineProps<{
+  videoData: VideoCardInterface[] // All video data for a search result
+}>()
+
+/**
+ * =============================================================================
+ *
+ * VARIABLES AND OTHER DATA
+ *
+ * =============================================================================
+ */
+
+const router = useRouter()
+
+/**
+ * =============================================================================
+ *
+ * FUNCTIONS
+ *
+ * =============================================================================
+ */
+
+const redirectToVideo = (id: string): void => {
+  router.push({ name: 'video', params: { id } })
+}
 </script>
 
 <template>
   <!-- Vertical Video Feed -->
-  <div class="flex flex-col space-y-4 p-4">
+  <div class="flex flex-col gap-4 p-4">
     <!-- Search Result Placeholder -->
-    <WideVideoCardComponent v-for="i in 5" :key="i" />
+    <WideVideoCardComponent
+      v-for="(item, index) in videoData"
+      :key="index"
+      :title="item.title"
+      :duration="item.duration"
+      :level="item.level"
+      :thumbnail="item.thumbnail"
+      :channel-name="item.channelName"
+      :views="item.views"
+      :release-time="item.releaseTime"
+      :tags="item.tags"
+      @clicked="redirectToVideo(item.id)"
+    />
   </div>
-
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
