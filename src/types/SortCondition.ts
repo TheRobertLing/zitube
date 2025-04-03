bfed086dc87c8760272d1f21909f7537c75551d6
Leo Tong <leo.tong@student.unsw.edu.au>
2025-04-03 18:37:54 +1100

    added types

---
+++ src/types/SortCondition.ts
@@ -0,0 +1,7 @@
+export type SortCondition = 'All' | 'Most Viewed' | 'Top Rated'
+export type SortDuration =
+  | 'All lengths'
+  | 'Under 10 min'
+  | '10-30 min'
+  | '30-60 min'
+  | 'Over 60 min'
